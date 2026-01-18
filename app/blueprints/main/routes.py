13be210ce314234ce18b79aab699c85eb7235089
Jamin <jamis03@zedat.fu-berlin.de>
2026-01-18 14:13:12 +0100

    test

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, url_for
 
 main_bp = Blueprint("main", __name__, template_folder="templates/main/")
 
-@main_bp.route("/")
-def home():
-    # Redirect root to the blog listing
-    return redirect(url_for("blog.list_posts"))
+# @main_bp.route("/")
+# def home():
+#     # Redirect root to the blog listing
+#     return redirect(url_for("blog.list_posts"))
